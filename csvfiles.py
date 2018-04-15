cecd3a4fb1a7698123eb70daa24ceab613e09685
SallyHabib <sally.hanen@student.guc.edu.eg>
2018-04-15 20:29:02 +0200

    date

--- csvfiles.py
+++ csvfiles.py
@@ -17,85 +17,85 @@ coll = db['facebook']
 #     out.writerows(b)
 #     a=[]
 #     b=[]
-# with open('users.csv', 'rb') as f:
-#     reader = csv.reader(f)
-#     for row in reader:
-#         faceID=row[2]
-#         export= db.posts.find({"users_id":faceID})
-#         out= csv.writer(open('user_posts_'+faceID+'.csv', 'wb'),delimiter=(','))
-#         b=[]
-#         a=[]
-#         for items in export:
-#             a.append(items['message'].encode("utf-8"))
-#             a.append(items['story'].encode("utf-8"))
-#             a.append(items['_id'].encode("utf-8"))
-#             a.append(items['created_time'])
-#             b.append(a)
-#             out.writerows(b)
-#             a=[]
-#             b=[]
-#         likes= db.likes.find({"user_id":faceID})
-#         out= csv.writer(open('user_likes_'+faceID+'.csv', 'wb'),delimiter=(','))
-#         c=[]
-#         d=[]
-#         for items in likes:
-#             c.append(items['name'].encode("utf-8"))
-#             c.append(items['about'].encode("utf-8"))
-#             c.append(items['category'].encode("utf-8"))
-#             d.append(c)
-#             out.writerows(d)
-#             c=[]
-#             d=[]
-#         events= db.events.find({"user_id":faceID})
-#         out= csv.writer(open('user_events_'+faceID+'.csv', 'wb'),delimiter=(','))
-#         e=[]
-#         f=[]
-#         for items in events:
-#             e.append(items['name'].encode("utf-8"))
-#             e.append(items['desc'].encode("utf-8"))
-#             e.append(items['status'].encode("utf-8"))
-#             f.append(e)
-#             out.writerows(f)
-#             e=[]
-#             f=[]
+with open('users.csv', 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        faceID=row[2]
+        export= db.posts.find({"users_id":faceID})
+        out= csv.writer(open('user_posts_'+faceID+'.csv', 'wb'),delimiter=(','))
+        b=[]
+        a=[]
+        for items in export:
+            a.append(items['message'].encode("utf-8"))
+            a.append(items['story'].encode("utf-8"))
+            a.append(items['_id'].encode("utf-8"))
+            a.append(items['created_time'])
+            b.append(a)
+            out.writerows(b)
+            a=[]
+            b=[]
+        likes= db.likes.find({"user_id":faceID})
+        out= csv.writer(open('user_likes_'+faceID+'.csv', 'wb'),delimiter=(','))
+        c=[]
+        d=[]
+        for items in likes:
+            c.append(items['name'].encode("utf-8"))
+            c.append(items['about'].encode("utf-8"))
+            c.append(items['category'].encode("utf-8"))
+            d.append(c)
+            out.writerows(d)
+            c=[]
+            d=[]
+        events= db.events.find({"user_id":faceID})
+        out= csv.writer(open('user_events_'+faceID+'.csv', 'wb'),delimiter=(','))
+        e=[]
+        f=[]
+        for items in events:
+            e.append(items['name'].encode("utf-8"))
+            e.append(items['desc'].encode("utf-8"))
+            e.append(items['status'].encode("utf-8"))
+            f.append(e)
+            out.writerows(f)
+            e=[]
+            f=[]
      
      
-posts= db.posts.find()
-out= csv.writer(open('user_posts.csv', 'wb'),delimiter=(','))
-b=[]
-a=[]
-for items in posts:
-    a.append(items['message'].encode('utf-8'))
-    a.append(items['story'].encode("utf-8"))
-    a.append(items['_id'].encode("utf-8"))
-    a.append(items['tags'])
-    a.append(items['created_time'])
-    b.append(a)
-    out.writerows(b)
-    a=[]
-    b=[]
-likes= db.likes.find()
-out= csv.writer(open('user_likes.csv', 'wb'),delimiter=(','))
-b=[]
-a=[]
-for items in likes:
-    a.append(items['name'].encode("utf-8"))
-    a.append(items['about'].encode("utf-8"))
-    a.append(items['category'].encode("utf-8"))
-    b.append(a)
-    out.writerows(b)
-    a=[]
-    b=[]
+# posts= db.posts.find()
+# out= csv.writer(open('user_posts.csv', 'wb'),delimiter=(','))
+# b=[]
+# a=[]
+# for items in posts:
+#     a.append(items['message'].encode('utf-8'))
+#     a.append(items['story'].encode("utf-8"))
+#     a.append(items['_id'].encode("utf-8"))
+#     a.append(items['tags'])
+#     a.append(items['created_time'])
+#     b.append(a)
+#     out.writerows(b)
+#     a=[]
+#     b=[]
+# likes= db.likes.find()
+# out= csv.writer(open('user_likes.csv', 'wb'),delimiter=(','))
+# b=[]
+# a=[]
+# for items in likes:
+#     a.append(items['name'].encode("utf-8"))
+#     a.append(items['about'].encode("utf-8"))
+#     a.append(items['category'].encode("utf-8"))
+#     b.append(a)
+#     out.writerows(b)
+#     a=[]
+#     b=[]
 
-events= db.events.find()
-out= csv.writer(open('user_events.csv', 'wb'),delimiter=(','))
-b=[]
-a=[]
-for items in events:
-    a.append(items['name'].encode("utf-8"))
-    a.append(items['desc'].encode("utf-8"))
-    a.append(items['status'].encode("utf-8"))
-    b.append(a)
-    out.writerows(b)
-    a=[]
-    b=[]
+# events= db.events.find()
+# out= csv.writer(open('user_events.csv', 'wb'),delimiter=(','))
+# b=[]
+# a=[]
+# for items in events:
+#     a.append(items['name'].encode("utf-8"))
+#     a.append(items['desc'].encode("utf-8"))
+#     a.append(items['status'].encode("utf-8"))
+#     b.append(a)
+#     out.writerows(b)
+#     a=[]
+#     b=[]
---
+++ date.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+import csv
+with open('user_posts_1641812829207516.csv') as File:
+    tfidfReader = csv.reader(File)
+    i=1
+    for row in tfidfReader:
+        datetime_object = datetime.strptime(row[3][:-4],"%Y-%m-%dT%H:%M:%S+")
+        month=datetime_object.month
+        year=datetime_object.year
+        day=datetime_object.day
+        hour=datetime_object.hour
+        minute=datetime_object.minute
+        sec=datetime_object.second
+        i+=1
+        
