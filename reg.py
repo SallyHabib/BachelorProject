7383dee3647326b1e834e109b990b3d1126db7ce
SallyHabib <sally.hanen@student.guc.edu.eg>
2018-04-26 16:24:03 +0200

    reg3

--- reg.py
+++ reg.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPRegressor
 from numpy.ma.core import ravel
+from sklearn.metrics import classification_report,confusion_matrix
 
 results=np.zeros([23,31])
 results2=[]
@@ -20,46 +21,66 @@ with open("user.csv") as csvfile:
     reader = csv.reader(csvfile) # change contents to floats
     for row in reader: # each row is a list
         data.append(row)
-fvs_lexical = np.zeros((23, 3), np.float64)
+fvs_lexical = np.zeros((23, 33), np.float64)
 k=0
 z=0
 while(k<23):
-    while(z<3):
+    while(z<33):
         fvs_lexical[k, z] = data[k][z]
         z+=1
     z=0
     k+=1
 
-# data2=[]
-# with open("ch.csv") as csvfile2:
-#     reader2 = csv.reader(csvfile2) # change contents to floats
-#     for row in reader2: # each row is a list
-#         data2.append(row)
-# # print(data2)
-# fvs_lexical2 = np.zeros((23, 1), np.float64)
-# kk=0
-# zz=0
-# while(kk<23):
-#     while(zz<1):
-#         fvs_lexical2[kk, zz] = data2[kk][zz]
-#         zz+=1
-#     zz=0
-#     kk+=1
+data2=[]
+with open("ch.csv") as csvfile2:
+    reader2 = csv.reader(csvfile2) # change contents to floats
+    for row in reader2: # each row is a list
+        data2.append(row)
+# print(data2)
+fvs_lexical2 = np.zeros((23, 1), np.float64)
+kk=0
+zz=0
+while(kk<23):
+    while(zz<1):
+        fvs_lexical2[kk, zz] = data2[kk][zz]
+        zz+=1
+    zz=0
+    kk+=1
 # print(fvs_lexical2)
 
 # print(fvs_lexical)
+data3=[]
+with open("test.csv") as csvfile3:
+    reader3 = csv.reader(csvfile3) # change contents to floats
+    for row in reader3: # each row is a list
+        data3.append(row)
+        # print(row)
+fvs_lexical3 = np.zeros((1, 33), np.float64)
+kkk=0
+zzz=0
+while(kkk<1):
+    while(zzz<33):
+        fvs_lexical3[kkk, zzz] = data3[kkk][zzz]
+        zzz+=1
+    zzz=0
+    kkk+=1
+# print(fvs_lexical3)
 wine = pd.read_csv('user.csv')
 yy=pd.read_csv('ch.csv')
-X = wine
-y = ravel(yy)
+X = fvs_lexical
+y = ravel(fvs_lexical2)
 print(y)
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 scaler = StandardScaler()
 scaler.fit(X_train)
 StandardScaler(copy=True, with_mean=True, with_std=True)
 X_train = scaler.transform(X_train)
+# print(X_train)
 X_test = scaler.transform(X_test)
-mlp = MLPRegressor(hidden_layer_sizes=(13,13,13),max_iter=5000)
+mlp = MLPRegressor(hidden_layer_sizes=(10,10,10),max_iter=5000)
 print(mlp.fit(X_train,y_train))
 predictions = mlp.predict(X_test)
-print(predictions)
+print(X_test)
+testing_scalar=scaler.transform(fvs_lexical3)
+testing=mlp.predict(testing_scalar)
+print(testing)
