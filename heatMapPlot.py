cee8e3c1d5072ffb7d0bd737d89e3e570570bbfa
SallyHabib <sally.hanen@student.guc.edu.eg>
2018-04-30 20:34:58 +0200

    heatmap trial

---
+++ heatMapPlot.py
@@ -0,0 +1,13 @@
+import numpy as np; 
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+np.random.seed(0)
+sns.set()
+# uniform_data = np.random.rand(10, 12)
+# ax = sns.heatmap(uniform_data)
+# flights = sns.load_dataset("flights")
+flights=pd.read_csv("good.csv")
+flights = flights.pivot("timing", "value","type")
+ax = sns.heatmap(flights)
+plt.show()
